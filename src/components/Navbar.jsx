7c0dc1c98473534dd9d3ce04eddfc410e2e280f6
Zaman00007 <shahidzaman75724@gmail.com>
2024-02-23 04:11:21 +0530

    planets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,9 +27,7 @@ const Navbar = () => {
             </ul>
           </div>
           <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-              Earth
-            </button>
+           
           </div>
         </div>
       </div>
