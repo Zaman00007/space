8cfef29c3d292bb4a70fd0d6bd15cbec85a26413
Aakif9866 <aakif9866@gmail.com>
2024-02-22 23:30:01 +0530

    earth

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import bgVideo from "./assets/earth-bg.mp4";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <div className="h-[700px] relative">
+        <video
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          className="fixed right-0 top-0 h-[700[px]] w-full object-cover z[-1]"
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Logo from "../assets/logo.png";
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm py-4 sm:py-4">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4 text-white font-bold text-2xl">
+            <img src={Logo} alt="" className="w-10" />
+            <span>S-EX</span>
+          </div>
+          <div className="text-white ">
+            <ul className="flex items-center gap-6 text-xl py-4">
+              <li>
+                <a href="#">Weather</a>
+              </li>
+              <li>
+                <a href="#"></a>
+              </li>
+              <li>
+                <a href="#">Galaxy</a>
+              </li>
+              <li>
+                <a href="#">Satelite Data</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+              Earth
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
