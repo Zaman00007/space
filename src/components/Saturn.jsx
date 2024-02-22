3fe2e5cc4a48b3cc04924c4b46fd37574836bd78
Aakif9866 <aakif9866@gmail.com>
2024-02-23 01:28:59 +0530

    all components made

---
+++ src/components/Saturn.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import bgVideo from "./assets/saturn-bg.mp4";
+import Navbar from "./components/Navbar";
+
+const Saturn = () => {
+  return (
+    <div>
+      <div className="h-[700px] relative">
+        <video
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          className="fixed right-0 top-0 h-[700[px]] w-full object-cover z[-1]"
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+        {/* <CosmoCast /> */}
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Saturn;
