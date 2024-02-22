13d327c55482d2ad272dcd7c0e1e6e76e7b0e911
Zaman00007 <shahidzaman75724@gmail.com>
2024-02-23 04:05:39 +0530

    earth

--- src/components/Earth.jsx
+++ src/components/Earth.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import bgVideo from "./assets/earth-bg.mp4";
-import Navbar from "./components/Navbar";
-import CosmoCast from "./components/CosmoCast";
-import Earth from "./components/Earth"
+import bgVideo from "../assets/earth-bg.mp4";
+import Navbar from "./Navbar";
 
 
-const App = () => {
+
+const Earth = () => {
 const API_URL = 'https://api.nasa.gov/DONKI/CMEAnalysis?startDate=2016-09-01&endDate=2016-09-30&mostAccurateOnly=true&speed=500&halfAngle=30&catalog=ALL&api_key=EMTkcLCSO1ZdgNMp6U1JGICVh1iqgg2yonmguwcc';
 
 
@@ -26,11 +25,19 @@ fetch(`${API_URL}`)
   return (
     <div>
       <div className="h-[700px] relative">
-        <Earth/>
+        <video
+          autoPlay={true}
+          loop={true}
+          muted={true}
+          className="fixed right-0 top-0 h-[700[px]] w-full object-cover z[-1]"
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+
         <Navbar />
       </div>
     </div>
   );
 };
 
-export default App;
+export default Earth;
